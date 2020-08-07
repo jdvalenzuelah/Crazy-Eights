491dcaea777fc777f5a8f873bff8f3af0c01c927
Josue Valenzuela <val171001@uvg.edu.gt>
2020-08-06 21:15:35 -0600

    Add basic parsing to dataclasses

--- game/card_deck/card.py
+++ game/card_deck/card.py
@@ -1,8 +1,27 @@
 from dataclasses import dataclass
 from card_deck.suits import Suits
-from card_deck.ranks import Ranks
+from card_deckranks import Ranks
+#from game_service.crazy_serializer.crazy_class import CrazyClass
 
 @dataclass
 class Card:
     rank: Ranks
     suit: Suits
+
+    @classmethod
+    def parse(self, data):
+        list_vals = data.split(',')
+
+        if len(list_vals) < 2:
+            return None
+        
+        rank = Ranks.from_string(list_vals[0])
+        suit = Suits.from_string(list_vals[1])
+
+        if not rank or not suit:
+            return None
+        
+        return Card(rank, suit)
+    
+    def serialize(self):
+        return f'{self.rank.value},{self.suit.value}'
