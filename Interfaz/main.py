d34c9d8cc83841b24a74710bf87941f90ae40f39
Marcos Gutierrez <33351664+MaaarcosG@users.noreply.github.com>
2020-08-10 10:26:05 -0600

    interfaz implementation

--- Interfaz/main.py
+++ Interfaz/main.py
@@ -1,10 +1,8 @@
 import eel
-
 @eel.expose
 
 def hello():
 	print('Hello World!')
 
 eel.init('www')
-eel.start('example.html')
-#https://github.com/BradySheehan/Crazy_Eights_live/blob/master/crazy_eights/Crazy8_2.html
+eel.start('index.html')
